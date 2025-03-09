97c729916f09525825f844bf315cbf1d970c2968
Lavanya Vaidya <lavanyavaidya@Lavanyas-MacBook-Air.local>
2025-03-09 21:32:36 +0530

    updated cases

--- application/static/script.js
+++ application/static/script.js
@@ -42,3 +42,31 @@ document.querySelectorAll('nav a').forEach(link => {
         }
     });
 });
+
+
+
+//Law script.js
+let slideIndex = 0;
+const slides = document.querySelectorAll('.slide');
+const totalSlides = slides.length;
+
+function showSlides() {
+    for (let i = 0; i < totalSlides; i++) {
+        slides[i].style.display = 'none';
+    }
+    slideIndex++;
+    if (slideIndex > totalSlides) { slideIndex = 1; }
+    slides[slideIndex - 1].style.display = 'block';
+    setTimeout(showSlides, 3000); // Change slide every 3 seconds
+}
+
+function moveSlide(n) {
+    slideIndex += n;
+    if (slideIndex > totalSlides) { slideIndex = 1; }
+    if (slideIndex < 1) { slideIndex = totalSlides; }
+    showSlides();
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    showSlides();
+});
